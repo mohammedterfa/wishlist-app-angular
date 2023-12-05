1aa46055e3c147a2eb2c2ac3e660809c57bb94e9
mohammedterfa <mohammedterfa@gmail.com>
2023-12-05 15:42:24 +0300

    make WishItem class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { WishItem } from 'src/shared/models/wishItem';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items = [
+    new WishItem('To Learn Angular'),
+    new WishItem('To Learn React', true),
+    new WishItem('To Learn Vue', true)
+  ];
   title = 'wishlist';
 }
---
+++ src/shared/models/wishItem.ts
@@ -0,0 +1,5 @@
+export class WishItem {
+  constructor(public wishList : string, public isComplate : boolean = false){
+
+  }
+}
